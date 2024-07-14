0efc85401dfba250158bf56804f08ed06be1eba7
norv <n139.2014@gmail.com>
2024-07-14 13:20:52 +0300

    first commit: created all logic + fix css/scss

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,35 @@
 import arrow from 'src/images/arrow.svg';
+import clsx from 'clsx';
 
 import styles from './ArrowButton.module.scss';
+import { SyntheticEvent } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+export type ArrowButtonProps = {
+	onClick?: () => void;
+	isOpen: boolean;
+};
 
-export const ArrowButton = () => {
+export const ArrowButton = ({ onClick, isOpen }: ArrowButtonProps) => {
+	const arrowClickHandler = () => {
+		onClick?.();
+	};
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, isOpen && styles.container_open)}
+			onClick={(e: SyntheticEvent) => {
+				e.stopPropagation();
+				arrowClickHandler();
+			}}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, isOpen && styles.arrow_open)}
+			/>
 		</div>
 	);
 };
